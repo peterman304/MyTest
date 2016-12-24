1c1ca5d6e7aa5e28006dee49e1c942a009978d4c
peterman304 <peterman304@yahoo.com>
2016-12-24 13:40:50 -0500

    local commit

--- sample.c
+++ sample.c
@@ -5,7 +5,7 @@ int main()
 	printf('hello world');
 
 	printf('this is a change to the master branch...');
-
+	// this is a local dev commit...
 	printf('this is the dev branch...yesss!!');
 	printf('git is totally awesome!!');
 	printf('now to to a pull request....');
