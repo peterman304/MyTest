59bae0fc73755930fabbe58bf383fbd4a5c42da9
peterman304 <peterman304@yahoo.com>
2016-12-24 13:28:03 -0500

    This is the first commit to the dev branch

--- sample.c
+++ sample.c
@@ -5,5 +5,8 @@ int main()
 	printf('hello world');
 
 	printf('this is a change to the master branch...');
+
+	printf('this is the dev branch...yesss!!');
+	printf('git is totally awesome!!');
 	return 0;
 }
